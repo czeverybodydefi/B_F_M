c121aea8534d22e6baca1b3f8d798b35be7ab286
czeverybodydefi <czeverybodydefi@gmail.com>
2022-10-27 14:41:12 +0800

    first commit

---
+++ scripts/deploy.py
@@ -0,0 +1,29 @@
+from symbol import funcdef
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import deploy_mocks, get_account, deploy_mocks,LOCAL_BLOCKCHAIN_ENVIRONMENTS
+
+
+def deploy_fund_me():
+    account = get_account()
+    # pass the price feed address to our fundme contract
+    #if we are on a persistent network like goerli, use the associated address
+    #otherwise, deploy mocks
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+        
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    print(f"FundMe deployed to {fund_me.address}")
+    return fund_me
+
+def main():
+    deploy_fund_me()
